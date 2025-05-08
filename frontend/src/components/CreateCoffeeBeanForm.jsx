2a87b659d688a41ca921d335a10c7db63234dbca
Katlin706 <katlin.hopkins706@gmail.com>
2025-05-08 15:52:34 -0500

    Create CreateCoffeeBeanForm.jsx

---
+++ frontend/src/components/CreateCoffeeBeanForm.jsx
@@ -0,0 +1,56 @@
+const CreateCoffeeBeanForm = ({ backendURL, refreshCoffeeBeans }) => {
+
+    return (
+        <>
+            <h2>Create a Coffee Bean</h2>
+
+            <form className='cuForm'>
+                <label htmlFor="create_coffeeBean_brandName">Brand: </label>
+                <input
+                    type="text"
+                    name="create_coffeeBean_brandName"
+                    id="create_coffeeBean_brandName"
+                    required
+                    maxLength={45}
+                />
+
+                <label htmlFor="create_coffeeBean_roastName">Roast Name: </label>
+                <input
+                    type="text"
+                    name="create_coffeeBean_roastName"
+                    id="create_coffeeBean_roastName"
+                    maxLength={45}
+                    required
+                />
+
+                <label htmlFor="create_coffeeBean_singleOriginCountry">Country of Origin (single origin only): </label>
+                <input
+                    type="text"
+                    name="create_coffeeBean_singleOriginCountry"
+                    id="create_coffeeBean_singleOriginCountry"
+                    maxLength={225}
+                />
+
+                <label htmlFor="create_coffeeBean_roastLevel">Roast Level: </label>
+                <input
+                    type="text"
+                    name="create_coffeeBean_roastLevel"
+                    id="create_coffeeBean_roastLevel"
+                    maxLength={45}
+                />
+
+                <label htmlFor="create_coffeeBean_providedTastingNotes">Tasting Notes: </label>
+                <input
+                    type="text"
+                    name="create_coffeeBean_providedTastingNotes"
+                    id="create_coffeeBean_providedTastingNotes"
+                    maxLength={500}
+                />
+
+                <input type="submit" />
+            </form>
+        </>
+    );
+};
+
+export default CreateCoffeeBeanForm;
