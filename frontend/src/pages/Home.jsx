b6b644cc57efc0e2cf9864bdb65e45ae28d16bf7
Thomas <kiss.thomas@gmail.com>
2025-05-08 15:31:25 -0700

    working merge

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,9 +1,10 @@
 function Home() {
     return (
         <>
-            <h1>Brew Review Home Page</h1>
+            <h1>Roast Review Home Page</h1>
             <div className="homepageDescription">
-                <p>Developer information and Project overview here.</p>
+                <p>RoastReview is a database driven website where users can review their experience with different single origin coffee beans and brewing methods.</p>
+                <p> Developed by Katlin Hopkins and Thomas Kiss</p>
             </div>
         </>
     )
