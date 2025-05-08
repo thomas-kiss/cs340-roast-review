681795ae25e0b6101d22d7717000a081158c27c4
Katlin706 <katlin.hopkins706@gmail.com>
2025-05-08 15:54:45 -0500

    Create UpdateVarietalsForm.jsx

---
+++ frontend/src/components/UpdateVarietalsForm.jsx
@@ -0,0 +1,31 @@
+const UpdateVarietalsForm = ({ varietals, backendURL, refreshVarietals }) => {
+    return (
+        <>
+            <h2>Update a User</h2>
+            <form className='cuForm'>
+                <label htmlFor="update_varietal_id">Varietal to Update: </label>
+                <select
+                    name="update_varietal_id"
+                    id="update_varietal_id"
+                >
+                    <option value="">Select a Varietal</option>
+                    {varietals.map((varietal) => (
+                        <option key={varietal.varietalID} value={varietal.varietalID}>
+                            {varietal.varietalID}
+                        </option>
+                    ))}
+                </select>
+
+                <label htmlFor="update_varietal_name">Name: </label>
+                <input
+                    type="text"
+                    name="update_varietal_name"
+                    id="update_varietal_name"
+                />
+                <input type="submit" />
+            </form>
+        </>
+    );
+};
+
+export default UpdateVarietalsForm;
