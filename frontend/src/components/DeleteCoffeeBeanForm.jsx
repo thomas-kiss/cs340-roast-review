ff11da36fcc82b975ca31297881038f7a0e5fab5
Katlin706 <katlin.hopkins706@gmail.com>
2025-05-08 15:53:21 -0500

    Create DeleteCoffeeBeanForm.jsx

---
+++ frontend/src/components/DeleteCoffeeBeanForm.jsx
@@ -0,0 +1,15 @@
+const DeleteCoffeeBeanForm = ({ rowObject, backendURL, refreshCoffeeBeans }) => {
+
+    return (
+        <td>
+            <form>
+                <button type='submit'>
+                    Delete
+                </button>
+            </form>
+        </td>
+
+    );
+};
+
+export default DeleteCoffeeBeanForm;
