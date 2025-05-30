eeb6cbc96ef7de59e6e3b88a89dcff2958f7ba78
Katlin Hopkins <hopkinka@classwork.engr.oregonstate.edu>
2025-05-30 16:36:37 -0700

    udpate varietal code

---
+++ backend/database/plsql.sql
@@ -0,0 +1,53 @@
+/* 
+Course: OSU CS340 Intro to Databases
+Group: Group 2
+Team Name: Team 2
+Project Title: Roast Review
+Group Members: Thomas Kiss, Katlin Hopkins
+*/
+
+
+/*
+Citation for CREATE BrewMethod Procedure 
+Date: 05/21/2025
+Adapted from provided canvas code:
+Implementing CUD operations in your app
+Source URL:https://canvas.oregonstate.edu/courses/1999601/pages/exploration-implementing-cud-operations-in-your-app?module_item_id=25352968
+*/
+
+
+-- CREATE BrewMethod Procedure
+
+DROP PROCEDURE IF EXISTS sp_CreateBrewMethod;
+DROP PROCEDURE IF EXISTS sp_UpdateVarietals;
+
+
+DELIMITER //
+
+CREATE PROCEDURE sp_CreateBrewMethod(
+    IN p_name VARCHAR(100), 
+    IN p_description TEXT,
+    OUT p_brewMethodID INT
+)
+BEGIN
+    INSERT INTO BrewMethods (name, description)
+    VALUES (p_name, p_description);
+
+    -- Get the ID of the newly inserted brew method
+    SELECT LAST_INSERT_ID() INTO p_brewMethodID;
+
+    -- Also return the ID directly
+    SELECT LAST_INSERT_ID() AS 'new_brew_method_id';
+END //
+
+
+
+
+-- UPDATE varietals PROC
+CREATE PROCEDURE sp_UpdateVarietals(IN p_id int, p_name varchar(45))
+
+BEGIN
+    UPDATE Varietals SET name = p_name WHERE varietalID= p_id; 
+END //
+
+DELIMITER ;
