bc37c1ddf766dc234671e8d2133c35d1d288ed69
Thomas Kiss <kiss.thomas@gmail.com>
2025-06-02 20:09:21 -0700

    Update DeleteUserForm.jsx

--- frontend/src/components/DeleteUserForm.jsx
+++ frontend/src/components/DeleteUserForm.jsx
@@ -1,3 +1,11 @@
+/*
+Citation for us of CS340 Starter Code 
+Date: 06/02/2025
+Adapted from provided canvas code:
+Implementing CUD operations in your app
+Source URL:https://canvas.oregonstate.edu/courses/1999601/pages/exploration-implementing-cud-operations-in-your-app?module_item_id=25352968
+*/
+
 /* Citation for use of AI Tools
 Date: 05/30/2025
 Prompts used to rewrite the DeleteUserForm to match clean style with minimal UI for deletion only
