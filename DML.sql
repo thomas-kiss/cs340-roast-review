311e1c2ce4c1b054dce3d0179b0aac107462e6a5
Katlin Hopkins <katlin.hopkins706@gmail.com>
2025-06-09 22:36:48 -0500

    Update DML.sql

--- DML.sql
+++ DML.sql
@@ -11,12 +11,19 @@ VALUES (@userNameInput, @emailInput, @firstNameInput, @lastNameInput, @locationI
  
 -- READ --
 -- get all users for the user page
-SELECT userID, userName, email, firstName, lastName, location, joinDate  
-FROM Users;
+ SELECT
+    userID AS "User ID",
+    userName AS "Username",
+    email AS "Email",
+    firstName AS "First Name",
+    lastName AS "Last Name",
+    location AS "Location",
+    joinDate AS "Join Date"
+ FROM Users;
  
 -- UPDATE --  
 -- get a single user's data for Update User form based on click of Update button on record's row
-SELECT userID, userName, email, firstName, lastName, location, joinDate  
+SELECT userName, email, firstName, lastName, location, joinDate  
 FROM Users  
 WHERE userID = @userID_selected_from_User_page;
  
@@ -40,12 +47,15 @@ VALUES (@nameInput, @descriptionInput);
  
 -- READ --  
 -- get all brew methods for the Brew Methods page
-SELECT brewMethodID, name, description  
+SELECT 
+   brewMethodID AS "Brew Method ID",
+   name AS "Brew Method Name",
+   description AS "Description"
 FROM BrewMethods;
  
 -- UPDATE --
 -- get a single brew method data for Update Brew Method form based on click of Update on the record's row
-SELECT brewMethodID, name, description  
+SELECT name, description  
 FROM BrewMethods  
 WHERE brewMethodID = @brewMethodID_selected_from_page;
  
@@ -69,9 +79,11 @@ SELECT userName FROM Users;
 -- get names from BrewMethods table to populate Brew Method dropdown
 SELECT name FROM BrewMethods;
  
- 
--- get roastName from CoffeeBeans table to populate Coffee Bean dropdown
+-- get roastName from CoffeeBeans table to populate roastName dropdown
 SELECT roastName FROM CoffeeBeans;
+
+-- get brandName from CoffeeBeans table to populate brandName dropdown 
+SELECT brandName FROM CoffeeBeans;
  
 -- add a new coffee review 
 INSERT INTO CoffeeReviews 
@@ -94,7 +106,7 @@ INSERT INTO CoffeeReviews
  @bodyInput, 
  @acidityInput, 
  @reviewNotesInput, 
- (SELECT coffeeBeanID from CoffeeBeans WHERE roastName = @roastName_selected_from_UpdateFormDropDown), 
+ (SELECT coffeeBeanID from CoffeeBeans WHERE roastName = @roastName_selected_from_UpdateFormDropDown AND brandName = @brandName_selected_from_UpdateFormDropDown), 
  (SELECT brewMethodID from BrewMethods WHERE name = @brewmethod_name_selected_from_UpdateFormDropDown), 
  (SELECT userID from Users WHERE userName = @userName_selected_from_UpdateFormDropDown)
  );
@@ -102,20 +114,20 @@ INSERT INTO CoffeeReviews
 -- READ --
 -- get all coffee reviews for Coffee Reviews page
 SELECT
-    CoffeeReviews.coffeeReviewID,
-    CoffeeReviews.reviewDate,
-    CoffeeReviews.aroma,
-    CoffeeReviews.flavor,
-    CoffeeReviews.afterTaste,
-    CoffeeReviews.body,
-    CoffeeReviews.acidity,
-    CoffeeReviews.reviewNotes,
-    Users.userID,
-    Users.userName,
-    CoffeeBeans.coffeeBeanID,
-    CoffeeBeans.roastName,
-    BrewMethods.brewMethodID,
-    BrewMethods.name
+    CoffeeReviews.coffeeReviewID AS "Review ID",
+    CoffeeReviews.reviewDate AS "Review Date",
+    CoffeeReviews.aroma AS "Aroma",
+    CoffeeReviews.flavor AS "Flavor",
+    CoffeeReviews.afterTaste AS "Aftertaste",
+    CoffeeReviews.body AS "Body",
+    CoffeeReviews.acidity AS "Acidity",
+    CoffeeReviews.reviewNotes AS "Notes",
+    Users.userID AS "User ID",
+    Users.userName AS "User Name",
+    CoffeeBeans.coffeeBeanID AS "Bean ID",
+    CoffeeBeans.roastName AS "Roast Name",
+    BrewMethods.brewMethodID AS "Brew Method ID",
+    BrewMethods.name AS "Brew Method"
 FROM CoffeeReviews
 JOIN Users ON CoffeeReviews.userID = Users.userID
 JOIN CoffeeBeans ON CoffeeReviews.coffeeBeanID = CoffeeBeans.coffeeBeanID
@@ -136,7 +148,7 @@ SET reviewDate = @reviewDateInput,
     body = @bodyInput,
     acidity = @acidityInput,
     reviewNotes = @reviewNotesInput,
-    coffeeBeanID = (SELECT coffeeBeanID from CoffeeBeans WHERE roastName = @roastName_selected_from_UpdateFormDropDown),
+    coffeeBeanID = (SELECT coffeeBeanID from CoffeeBeans WHERE roastName = @roastName_selected_from_UpdateFormDropDown AND brandName = @brandName_selected_from_UpdateFormDropDown),
     brewMethodID = (SELECT brewMethodID from BrewMethods WHERE name = @brewmethod_name_selected_from_UpdateFormDropDown),
     userID = (SELECT userID from Users WHERE userName = @userName_selected_from_UpdateFormDropDown)
 WHERE coffeeReviewID = @coffeereviewID_selected_from_CoffeeReviews_page;
@@ -157,13 +169,20 @@ INSERT INTO CoffeeBeans (brandName, roastName, singleOriginCountry, roastLevel, 
 -- Read --
   
 --get all coffee beans for the Coffee Bean page
-SELECT coffeeBeanID, brandName, roastName, singleOriginCountry, roastLevel, providedTastingNotes FROM CoffeeBeans;
+SELECT 
+    coffeeBeanID as "Coffee Bean ID",
+    brandName as "Brand Name", 
+    roastName as "Roast Name", 
+    singleOriginCountry as "Origin", 
+    roastLevel as "Roast Level", 
+    providedTastingNotes as "Provided Tasting Notes"
+FROM CoffeeBeans
+ORDER BY coffeeBeanID ASC;
 
 -- Update --
 
 --get a single coffee bean record based on the click of Update button on record's row
 SELECT 
- coffeeBeanID, 
  brandName, 
  roastName, 
  singleOriginCountry, 
@@ -192,7 +211,11 @@ INSERT INTO Varietals (name) VALUES (@nameInput);
 -- Read --
 
 --get all varietals for the Varietals page 
-SELECT varietalID, name from Varietals;
+SELECT 
+    varietalID as "Varietal ID", 
+    name as "Name"
+FROM Varietals
+ORDER BY varietalID ASC;
 
 -- Update --
 
@@ -219,27 +242,52 @@ INSERT INTO CoffeeBeansVarietals (coffeeBeanID, varietalID) VALUES (@coffeeBeanI
 -- Read --
 
 --get all varietals for the Varietals page 
-SELECT
- CoffeeBeansVarietals.coffeeBeanVarietalID, 
- CoffeeBeansVarietals.coffeeBeanID, 
- CoffeeBeans.brandName, 
- CoffeeBeans.roastName, 
- CoffeeBeansVarietals.varietalID,
- Varietals.name
- FROM CoffeeBeansVarietals
- JOIN CoffeeBeans on CoffeeBeansVarietals.coffeeBeanID = CoffeeBeans.coffeeBeanID
- JOIN Varietals on CoffeeBeansVarietals.varietalID = Varietals.varietalID;
+SELECT 
+    CoffeeBeansVarietals.coffeebeanvarietalID as "Coffee Bean by Varietal Relationship ID",
+    CoffeeBeans.coffeeBeanID as "Coffee Bean ID", 
+    CoffeeBeans.brandName as "Brand Name", 
+    CoffeeBeans.roastName as "Roast Name", 
+    Varietals.varietalID as "Varietal ID",
+    Varietals.name as "Varietal Name" 
+FROM CoffeeBeans
+JOIN CoffeeBeansVarietals on CoffeeBeans.coffeeBeanID = CoffeeBeansVarietals.coffeeBeanID
+JOIN Varietals on Varietals.varietalID = CoffeeBeansVarietals.varietalID 
+ORDER BY CoffeeBeansVarietals.coffeebeanvarietalID ASC;
+
 
 -- Update --
 
---get a list of coffee bean brand names for the drop down choices when updating the coffeebeanvarietal relationship
-SELECT brandName FROM CoffeeBeans;
+-- get brands for the BrandName drop down on the CoffeeBeansvarietals page
+SELECT DISTINCT 
+brandName as "Brand Name"
+FROM CoffeeBeans
+ORDER BY brandName ASC; 
 
---get a list of coffee bean roast names for the drop down choices when updating the coffeebeanvarietal relationship
-SELECT roastName FROM CoffeeBeans;
+-- get a subset of brands for the BrandName drop down based on if roastName drop down is selected on CoffeeBeansVarietals page
+SELECT DISTINCT 
+brandName as "Brand Name"
+FROM CoffeeBeans
+WHERE roastName = @roastName
+ORDER BY brandName ASC; 
+
+
+-- get roast names for the RoastName drop down on the CoffeeBeansvarietals page
+SELECT DISTINCT 
+roastName as "Roast Name"
+FROM CoffeeBeans
+ORDER BY roastName ASC; 
+
+-- get a subset of roast names for the RoastName drop down based on if brandName drop down is selected on CoffeeBeansVarietals page
+SELECT DISTINCT 
+roastName as "Roast Name"
+FROM CoffeeBeans
+WHERE  brandName = @brandName
+ORDER BY brandName ASC; 
 
 --get a list of varietal names for the drop down choices when updating the coffeebeanvarietal relationship
-SELECT name FROM Varietals;
+SELECT DISTINCT
+ name 
+FROM Varietals;
 
  
 --get data for the coffeebeanvarietal that was chosen from the Update button on the record's row
